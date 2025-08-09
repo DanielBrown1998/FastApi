ce53a78239d47392136bdd5c12023b9e690bda04
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-08-09 00:08:12 -0300

    Add initial FastAPI application and test setup

---
+++ test/core/test_app.py
@@ -0,0 +1,11 @@
+from http import HTTPStatus
+from fastapi.testclient import TestClient
+from core.app import app
+
+
+def test_root_would_return_hello_world():
+    client = TestClient(app)
+    response = client.get("/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json == {"message": 'Hello, World'}
